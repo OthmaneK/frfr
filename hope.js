f0a854662fe85f37335376c9871898b313a65782
OthmaneK <40889968+OthmaneK@users.noreply.github.com>
2018-07-10 16:02:20 +0000

    Add files via upload

---
+++ hope.js
@@ -0,0 +1,14 @@
+function yassina() {
+	document.getElementById('lol').innerHTML = "7eyd 7eyda berra";
+
+}
+function doktor() {
+	    if (document.getElementById("frfr").src == "https://images.unsplash.com/photo-1531185891031-0006616b31fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e711d949230c39bc58b66f6d5d6eb372&auto=format&fit=crop&w=500&q=60") 
+        {
+            document.getElementById("frfr").src = "https://images.unsplash.com/photo-1531185891031-0006616b31fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e711d949230c39bc58b66f6d5d6eb372&auto=format&fit=crop&w=500&q=60";
+        }
+        else
+        {
+            document.getElementById("frfr").src = "https://images.unsplash.com/photo-1531185891031-0006616b31fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e711d949230c39bc58b66f6d5d6eb372&auto=format&fit=crop&w=500&q=60";
+        }
+    }
